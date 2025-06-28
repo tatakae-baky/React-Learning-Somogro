4189558a7c4fd709532430697ab1fd74a52cbe37
J.AL BAKY <jubairalbaky12@gmail.com>
2025-06-28 19:03:50 +0600

    add visit button functionality to Country component

--- World-tour/src/components/country/Country.jsx
+++ World-tour/src/components/country/Country.jsx
@@ -1,17 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import "./country.css";
 
 const Country = ({ country }) => {
   const { name, flags, region, population, ccn3 } = country;
 
+  const [isVisited, setIsVisited] = useState(false)
+
+  const handleVisit = () => {
+    setIsVisited(!isVisited)
+  }
+
   return (
     <div className="country">
       <img src={flags.png} alt="" />
-      <h3>Name : {name.common} </h3>
+      <h3>Country : {name.common} </h3>
       <div className="country-details">
         <p>Region: {region} </p>
         <p>Population : {population} </p>
       </div>
+      <button onClick={handleVisit}>{isVisited? "Visited" : "Visit"}</button>
     </div>
   );
 };
