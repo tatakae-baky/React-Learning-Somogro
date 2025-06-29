c9ed8b26130c40bf76b3c5d4cac339d7cdecf236
J.AL BAKY <jubairalbaky12@gmail.com>
2025-06-30 01:28:05 +0600

    feat: enhance Bottle component with stock management and styling

--- Bottle-collection/src/components/Bottle/Bottle.jsx
+++ Bottle-collection/src/components/Bottle/Bottle.jsx
@@ -1,14 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Bottle.css";
 
 const Bottle = ({ bottle }) => {
-  const { name, price, id, img } = bottle;
+  const { name, price, id, img, stock } = bottle;
+  const [purchase, setPurchase] = useState(false);
+  const [currentStock, setCurrentStock] = useState(stock);
+
+  // manage purchasing and stock controlling
+  const handlePurchaseAndStock = () => {
+    setPurchase(!purchase);
+    if (!purchase) {
+      const newStock = currentStock - 1;
+      setCurrentStock(newStock);
+    } else {
+      setCurrentStock(stock);
+    }
+  };
+
   return (
     <div className="bottle">
       <h3>{name} </h3>
       <img src={img} alt="" />
-      <p>Price: {price}</p>
-      <button>Purchase</button>
+      <div className="price-stock">
+        <p>Price: {price}</p>
+        <p>Stock: {currentStock} </p>
+      </div>
+      <button onClick={handlePurchaseAndStock}>
+        {purchase ? "Purchased" : "Purchase"}
+      </button>
     </div>
   );
 };
