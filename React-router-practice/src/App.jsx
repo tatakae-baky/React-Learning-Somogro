d81e12fc82a11b11f2be07c6fb2811685214157b
J.AL BAKY <jubairalbaky12@gmail.com>
2025-07-05 09:46:18 +0600

    formatting

--- React-router-practice/src/App.jsx
+++ React-router-practice/src/App.jsx
@@ -1,13 +1,9 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
-import { Link, Outlet } from "react-router-dom";
+import { Outlet } from "react-router-dom";
 import Header from "./component/Header/Header";
 import Footer from "./component/Footer/Footer";
 
 function App() {
-  const [count, setCount] = useState(0);
 
   return (
     <>
