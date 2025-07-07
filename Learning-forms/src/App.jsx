2dcb681671369846edbd378f64828ca33b739c5d
J.AL BAKY <jubairalbaky12@gmail.com>
2025-07-07 08:40:56 +0600

    implemented form functionality

--- Learning-forms/src/App.jsx
+++ Learning-forms/src/App.jsx
@@ -1,18 +1,11 @@
 import './App.css'
+import Forms from './components/Forms/Forms'
 
 function App() {
   return (
     <>
       <h1 className='text-3xl font-bold m-4'>Learning React Forms</h1>
-      <form className='flex flex-col gap-2 m-4 w-1/2 mx-auto'>
-        <label htmlFor="name">Name</label>
-        <input className='border-2 border-gray-300 rounded-md p-2' type="text" id="name" name="name" />
-        <label htmlFor="email">Email</label>
-        <input className='border-2 border-gray-300 rounded-md p-2' type="email" id="email" name="email" />
-        <label htmlFor="password">Password</label>
-        <input className='border-2 border-gray-300 rounded-md p-2' type="password" id="password" name="password" />
-        <button className='bg-blue-500 text-white p-2 rounded-md' type="submit">Submit</button>
-      </form>
+      <Forms></Forms>
     </>
   )
 }
---
+++ Learning-forms/src/components/Forms/Forms.jsx
@@ -0,0 +1,71 @@
+import React, { useState } from "react";
+
+const Forms = () => {
+  const [form, setForm] = useState({
+    name: "",
+    email: "",
+    password: "",
+    confirmPassword: "",
+    phone: "",
+  });
+
+  const handleChange = e => {
+    setForm({...form, [e.target.name]: e.target.value})
+  }
+
+  const handleSubmit = e => {
+    e.preventDefault()
+    if(form.password !== form.confirmPassword){
+      alert("Password does not match")
+      return
+    }
+  }
+
+  return (
+    <form onSubmit={handleSubmit} className="flex flex-col gap-2 m-4 w-1/2 mx-auto">
+      <label>
+        Enter Your Name:
+        <input className="border-2 border-gray-300 p-2 rounded-md" type="text" 
+        name="name"
+        value={form.name}
+        onChange={handleChange}
+        />
+      </label>
+      <label>
+        Enter Your Email:
+        <input className="border-2 border-gray-300 p-2 rounded-md" type="email" 
+        name="email"
+        value={form.email}
+        onChange={handleChange}
+        />
+      </label>
+      <label>
+        Enter Your Password:
+        <input className="border-2 border-gray-300 p-2 rounded-md" type="password" 
+        name="password"
+        value={form.password}
+        onChange={handleChange}
+        />
+      </label>
+      <label>
+        Confirm Your Password:
+        <input className="border-2 border-gray-300 p-2 rounded-md" type="password" 
+        name="confirmPassword"
+        value={form.confirmPassword}
+        onChange={handleChange}
+        />
+      </label>
+      <label>
+        Enter Your Phone Number:
+        <input className="border-2 border-gray-300 p-2 rounded-md" type="number" 
+        name="phone"
+        value={form.phone}
+        onChange={handleChange}
+        />
+      </label>
+      <button type="submit" className="bg-blue-500 text-white p-2 rounded-md w-1/2 mx-auto">Submit</button>
+    </form>
+  );
+};
+
+export default Forms;
