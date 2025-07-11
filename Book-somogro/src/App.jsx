d92d59271a8a690d6479801821c71850757ea17a
J.AL BAKY <jubairalbaky12@gmail.com>
2025-07-11 22:04:46 +0600

    feat: initialize new 'Book Somogro' React project

---
+++ Book-somogro/src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div className="p-8 ">
+      <h1 className="text-4xl font-bold mb-4">Hello World</h1>
+    </div>
+  );
+};
+
+export default App;
