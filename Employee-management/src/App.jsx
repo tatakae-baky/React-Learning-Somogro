7a8561c070dadd28efe6f59b10dd289b0fb8be29
J.AL BAKY <jubairalbaky12@gmail.com>
2025-08-01 21:58:22 +0600

    Employee management project setup

---
+++ Employee-management/src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+
+  return (
+    <>
+      <h1 className='text-3xl text-center mt-10 text-primary-color font-normal'>Employee Management</h1>
+    </>
+  )
+}
+
+export default App
